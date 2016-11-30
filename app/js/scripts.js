5b25d0ee72e47fb54a27932f36dcc00157dd8e64
Christian Javan <christianjavan@hotmail.com>
2016-11-29 18:04:37 -0800

    Added new projects to Json File and added EmailJS support

--- app/js/scripts.js
+++ app/js/scripts.js
@@ -1,3 +1,4 @@
+
 function openEmailForm(){
   if(document.getElementById("contact-form").className != "open-form"){
     document.getElementById("contact-form").className += "open-form";
@@ -11,22 +12,69 @@ function closeEmailForm(){
   document.getElementById("+").className = "close-form";
 }
 
+function addClass(el, className){
+  if(el.classList)
+    el.classList.add(className);
+  else
+    el.className += ' ' + className;
+}
+
+function removeClass(el, className){
+  if (el.classList)
+    el.classList.remove(className);
+  else
+    el.className = el.className.replace(new RegExp('(^|\\b)' +
+    className.split(' ').join('|') + '(\\b|$)', 'gi'), ' ');
+}
+function afterError(el,tx){
+  setTimeout(function(){ tx.innerHTML = "Mandame un email"; removeClass(el,'background-error');  }, 3000);
+}
+
+function afterSucces(tx){
+  setTimeout(function(){ tx.innerHTML = "Mandame un email"; }, 3000);
+}
+
+function changeText(el, string){
+  el.innerHTML = string;
+}
 function sendEmail(){
+  el = document.getElementById('contact-form');
+  tx = document.getElementById('form-header-text');
+  bt = document.getElementById('send-button');
   var params = new URLSearchParams();
-
-  params.append('nombre', document.getElementById("nombre").value);
-  params.append('email', document.getElementById("email").value);
-  params.append('telefono', document.getElementById("telefono").value);
-  params.append('mensaje', document.getElementById("mensaje").value);
-
-  axios.post('php/send-email.php', params
-  )
-  .then(function (response) {
-    console.log(response);
-  })
-  .catch(function (error) {
-    console.log(error);
-  });
+  var nombre = document.getElementById('nombre').value;
+  var email = document.getElementById('email').value;
+  var telefono = document.getElementById('telefono').value;
+  var mensaje = document.getElementById('mensaje').value;
+  //console.log(nombre);
+  if(nombre === "" || email === "" || mensaje === "" || telefono === ""){
+    addClass(el, 'background-error');
+    tx.innerHTML = "Todos los campos son obligatorios";
+    afterError(el,tx);
+  } else{
+    params.append('email', email);
+    changeText(bt, 'Enviando...');
+    axios.post('php/verify-email.php', params)
+      .then(function(r){
+        if(r.data == 1){
+          emailjs.send("gmail","christianjavan_com",{nombre: nombre, email: email, telefono: telefono, mensaje: mensaje})
+          .then(function(response) {
+             //console.log("SUCCESS. status=%d, text=%s", response.status, response.text);
+             document.getElementById("mail-form").reset();
+             changeText(tx,'Email enviado!');
+             afterSucces(tx);
+             changeText(bt,'Enviar');
+          }, function(err) {
+             //console.log("FAILED. error=", err);
+          });
+        }else{
+          addClass(el, 'background-error');
+          tx.innerHTML = "El email no es valido"
+          setTimeout(function(){ tx.innerHTML = "Mandame un email"; removeClass(el,'background-error'); }, 3000);
+          console.log('nada');
+        }
+      });
+  }
 }
 
 /*!
